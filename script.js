98af5c4100d6b29cfa27a49dd56c178384266aa9
johnzama <johnsparta2121@gmail.com>
2024-10-09 18:34:17 -0400

    inita-files

---
+++ script.js
@@ -0,0 +1,48 @@
+// script.js
+
+const grid = document.querySelector('.grid');
+const width = 20; // 20x20 grid
+const layout = [
+    // 0: empty, 1: wall, 2: dot, 3: pac-man
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1,
+    1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1,
+    1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1,
+    1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1,
+    1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1,
+    1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1,
+    1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1,
+    1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1,
+    1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1,
+    1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1,
+    1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1,
+    1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+]
+
+const squares = [];
+
+// Create your grid
+function createBoard() {
+    for (let i = 0; i < layout.length; i++) {
+        const square = document.createElement('div');
+        grid.appendChild(square);
+        squares.push(square);
+
+        // Add layout to the board
+        if (layout[i] === 0) {
+            squares[i].classList.add('empty');
+        } else if (layout[i] === 1) {
+            squares[i].classList.add('wall');
+        } else if (layout[i] === 2) {
+            squares[i].classList.add('dot');
+        } else if (layout[i] === 3) {
+            squares[i].classList.add('pac-man');
+        }
+    }
+}
+
+createBoard();
+
